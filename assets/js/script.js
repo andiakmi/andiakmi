ab31ae508615bd9b01f098f3e0934f31a774ea4d
andinaeta <mikorlk4328@gmail.com>
2020-08-09 00:52:12 +0700

    updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,3 +15,24 @@ $(function () {
     lastYPos = yPos;
   });
 });
+
+const menuBtn = document.querySelector(".menu-btn");
+let menuOpen = false;
+menuBtn.addEventListener("click", () => {
+  if (!menuOpen) {
+    menuBtn.classList.add("open");
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove("open");
+    menuOpen = false;
+  }
+});
+
+const navLink = document.querySelectorAll(".nav-link");
+for (let i = 0; i < navLink.length; i++) {
+  navLink[i].addEventListener("click", () => {
+    if (menuOpen) {
+      menuBtn.click();
+    }
+  });
+}
