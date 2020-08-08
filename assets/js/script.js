545346f61e3b6cee0ca2b29996855d16c0dbfecc
andinaeta <mikorlk4328@gmail.com>
2020-08-09 02:20:18 +0700

    updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,3 +32,36 @@ for (let i = 0; i < navLink.length; i++) {
     }
   });
 }
+
+const words = ["A Student.", "Full stack Developer."];
+
+let cursor = gsap.to(".js-cursor", {
+  opacity: 0,
+  ease: "power2.inOut",
+  repeat: -1,
+});
+let masterTl = gsap.timeline({ repeat: -1 }).pause();
+let boxTl = gsap.timeline();
+
+boxTl
+  .to(".box", { duration: 1, width: "17vw", delay: 0.5, ease: "power4.inOut" })
+  .from(".hi", { duration: 1, y: "7vw", ease: "power3.out" })
+  .to(".box", {
+    duration: 1,
+    height: "7vw",
+    ease: "elastic.out",
+    onComplete: () => masterTl.play(),
+  })
+  .to(".box", {
+    duration: 2,
+    autoAlpha: 0.7,
+    yoyo: true,
+    repeat: -1,
+    ease:
+      "rough({ template: none.out, strength:  1, points: 20, taper: 'none', randomize: true, clamp: false})",
+  });
+words.forEach((word) => {
+  let tl = gsap.timeline({ repeat: 1, yoyo: true, repeatDelay: 1 });
+  tl.to(".js-text", { duration: 1, text: word });
+  masterTl.add(tl);
+});
